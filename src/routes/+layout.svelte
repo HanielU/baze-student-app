fd284094703fc8dd6c4e7511338bfff440b1b617
Haniel Ubogu <hanieltobi@gmail.com>
2022-11-09 18:47:17 +0100

    moved swipe interaction to a component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,73 +3,23 @@
   import "$styles/css/app.css";
   import "uno.css";
 
+  import Footer from "./components/navigation/Footer.svelte";
   import Header from "./components/navigation/Header.svelte";
   import Sidebar from "./components/navigation/Sidebar.svelte";
-  import Footer from "./components/navigation/Footer.svelte";
+  import SwipeInteraction from "./components/SwipeInteraction.svelte";
   import type { LocalNotificationsPlugin } from "@capacitor/local-notifications";
   import type { PermissionState } from "@capacitor/core";
   import { Device } from "@capacitor/device";
   import { hslToHex } from "$lib/utils";
   import { navigationStack } from "$lib/stores";
   import { onMount } from "svelte";
-  import { sidebarOpen } from "$lib/stores";
-  import { themeVars } from "$styles/vanilla/theme.css";
   import { page } from "$app/stores";
+  import { themeVars } from "$styles/vanilla/theme.css";
 
   let LocalNotifications: LocalNotificationsPlugin | undefined;
   let notificationsPermission: PermissionState | undefined;
 
   let footerHeight: number;
-
-  let dragging = false;
-  let startX = 0;
-  let startY = 0;
-  let currentX = 0;
-  let currentY = 0;
-
-  let movedXBy = 0;
-  let movedYBy = 0;
-
-  function getCurrentPos(e: Event) {
-    return e.type.includes("mouse") ? (e as MouseEvent) : (e as TouchEvent)?.touches?.[0];
-  }
-
-  function startDrag(e: MouseEvent | TouchEvent) {
-    const target = e.target as HTMLElement;
-    if (target.parentElement?.classList.contains("illegal-scroll")) return;
-
-    dragging = true;
-    ({ clientX: startX, clientY: startY } = getCurrentPos(e));
-  }
-
-  function handleDrag(e: MouseEvent | TouchEvent) {
-    if (!dragging) return;
-    document.documentElement.style.userSelect = "none";
-    ({ clientX: currentX, clientY: currentY } = getCurrentPos(e));
-  }
-
-  function stopDrag() {
-    dragging = false;
-    document.documentElement.style.userSelect = "initial";
-
-    movedXBy = currentX - startX;
-    movedYBy = currentY - startY;
-
-    // console.log({ movedXBy, movedYBy });
-
-    // makes sure the screen has actually been swiped through
-    // if screen has been swiped through then currentX and currentY
-    // will be something other than 0
-    if (currentX !== 0 && currentY !== 0) {
-      if (movedXBy >= 80 && movedYBy >= -60 && movedYBy <= 60 && !$sidebarOpen) {
-        $sidebarOpen = true;
-      } else if (movedXBy <= -80 && $sidebarOpen) {
-        $sidebarOpen = false;
-      }
-    }
-
-    currentX = startX = currentY = startY = 0;
-  }
 
   onMount(async () => {
     const { platform } = await Device.getInfo();
@@ -133,11 +83,11 @@
   });
 </script>
 
-<svelte:window on:touchstart={startDrag} on:touchmove={handleDrag} on:touchend={stopDrag} />
-
 <svelte:head>
   <title>Baze University Student Portal</title>
 </svelte:head>
+
+<SwipeInteraction />
 
 <div id="app" class="relative h-screen w-full overflow-x-hidden" style:--footerH="{footerHeight}px">
   <Sidebar />
